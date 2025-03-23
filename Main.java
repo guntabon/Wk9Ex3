36ff2da3e22b576f041c0df9a42c8c878427de6a
g0364875@amarillocollege.com <g0364875@amarillocollege.com>
2025-03-23 18:45:51 -0500

    Second commit

--- Main.java
+++ Main.java
@@ -3,16 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
+        //This line creates a scanner for user input later
         Scanner scanner = new Scanner(System.in);
 
+        //This line welcomes the user to the application
         System.out.println("Welcome to Temperature Converter");
 
+        //This line prompts the user to enter the temperature
         System.out.println("Enter the temperature: ");
 
+        //This line creates the temp double and assigns it the user entered value
         double temp = scanner.nextDouble();
 
+        //This line creates the celsius double and assigns it to the value of temp -32 * 5/9
         double celsius = (5.0/9.0) * (temp - 32);
 
+        //THis line prints out the temperature and its equivalent celsius output
         System.out.println(temp + "F degree is equivalent to " + celsius + " C");
     }
 }
